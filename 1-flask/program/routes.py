4905af62059ada0f20ebb7ceb874e06003bce6ab
Bert Bullough <bert@bertbullough.com>
2019-05-04 21:54:55 -0600

    adds template calls to routes.py

--- 1-flask/program/routes.py
+++ 1-flask/program/routes.py
@@ -1,6 +1,7 @@
+from flask import render_template
 from program import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello Pythonistas'
+    return render_template('index.html')
